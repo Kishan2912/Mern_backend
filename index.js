8506c767bbfbbbe79a050dc4dc4d2d44dfa660b8
Kishanmorsebiz <83392319+Kishan2912@users.noreply.github.com>
2023-03-29 19:27:43 +0530

    Add Frontend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const connection = require("./db");
 const User = require('./models/user')
 const authRoutes=require('./routes/auth')
+const path=require("path")
 
 // database connection
 connection()
@@ -12,6 +13,14 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/", authRoutes);
+
+app.use(express.static(path.join(__dirname, "../Mern_Frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(
+     path.resolve(__dirname, "../", "Mern_Frontend", "build", "index.html")
+  )
+);
 
 app.listen(1337, () => {
 	console.log('Server started on 1337')
