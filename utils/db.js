269e3834083a9c1a01f7a26194f50d740064a430
Kishanmorsebiz <83392319+Kishan2912@users.noreply.github.com>
2023-03-30 02:55:38 +0530

    change1

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv');
-dotenv.config();
-
 
 module.exports= ()=>{
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect('mongodb://127.0.0.1:27017/smart_design')
 
 }
